dfac36253de1f49250a63d8d7915a592208288df
krisszcode <nagy.ace.krisztian@gmail.com>
2020-09-08 16:29:55 +0200

    css to card components

--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -7,6 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
+  properties: Array<any> = [
+    {
+      "Id":1,
+      "Name":"Birla House",
+      "Type":"House",
+      "Price":12000
+     },{
+      "Id":2,
+      "Name":"Villa House",
+      "Type":"House",
+      "Price":15000
+     },{
+      "Id":3,
+      "Name":"White House",
+      "Type":"House",
+      "Price":18000
+     },{
+      "Id":4,
+      "Name":"White House",
+      "Type":"House",
+      "Price":18000
+     },{
+      "Id":5,
+      "Name":"White House",
+      "Type":"House",
+      "Price":18000
+     },{
+      "Id":6,
+      "Name":"White House",
+      "Type":"House",
+      "Price":18000
+     },
+]
+
   constructor() { }
 
   ngOnInit(): void {
