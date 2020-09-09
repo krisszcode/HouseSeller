e0175c07e8dfca3796762d17e9391da97f14751f
Krisztian <nagy.ace.krisztian@gmail.com>
2020-09-09 20:33:47 +0200

    passing data through an API

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { PropertyCardComponent } from './property/property-card/property-card.component';
@@ -7,14 +8,15 @@ import { PropertyListComponent } from './property/property-list/property-list.co
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
     PropertyCardComponent,
     PropertyListComponent,
       NavBarComponent
    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClientJsonpModule, HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-property-list',
@@ -7,43 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
-  properties: Array<any> = [
-    {
-      "Id":1,
-      "Name":"Birla House",
-      "Type":"House",
-      "Price":12000
-     },{
-      "Id":2,
-      "Name":"Villa House",
-      "Type":"House",
-      "Price":15000
-     },{
-      "Id":3,
-      "Name":"White House",
-      "Type":"House",
-      "Price":18000
-     },{
-      "Id":4,
-      "Name":"White House",
-      "Type":"House",
-      "Price":18000
-     },{
-      "Id":5,
-      "Name":"White House",
-      "Type":"House",
-      "Price":18000
-     },{
-      "Id":6,
-      "Name":"White House",
-      "Type":"House",
-      "Price":18000
-     },
-]
+  properties: any;
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get('data/properties.json').subscribe(
+      data=>{
+        this.properties = data;
+      }
+    );
   }
 
 }
